ad9dc8fd4fc39e5ebfd0ab9f66a950b807ec93c4
Qwexaro <128168133+Qwexaro@users.noreply.github.com>
2025-03-10 11:36:53 +0300

    Add files via upload

--- Planets/RPO1_Ivanov_Sergey_planets.cpp
+++ Planets/RPO1_Ivanov_Sergey_planets.cpp
@@ -6,7 +6,9 @@ class Planet
 {
 	std::string name;
 	double size, height, weight;
+
 public:
+	
 	Planet()
 	{
 		name = "unknow"; size = -1; height = -1; weight = -1;
@@ -18,27 +20,104 @@ public:
 		this->height = _height;
 		this->weight = _weight;
 	}
-	void printInfo(int i)
+	void printInfo()
 	{
-		std::cout << "»м€ планеты: " << name  << " " << i << ", размер планеты: " << size
-			<< ", высота планеты: " << height << ", ширина планеты: " << weight << std::endl;
+		std::cout << "»м€ планеты: " << getName() << ", размер планеты: " << getSize()
+			<< ", высота планеты: " << getHeight() << ", ширина планеты: " << getWeight() << std::endl;
 	}
+
+
+	std::string getName() const 
+	{
+		return name;
+	}
+
+	double getSize() const
+	{
+		return size;
+	}
+
+	double getHeight() const
+	{
+		return height;
+	}
+
+	double getWeight() const
+	{
+		return weight;
+	}
+
+	Planet& setName(std::string _name)
+	{
+		name = _name; return *this;
+	}
+
+	Planet& setSize(double _size)
+	{
+		if (_size > 0 && _size < 1000) { size = _size; }return *this;
+	}
+
+	Planet& setHeight(double _height)
+	{
+		if (_height > 0 && _height < 1000) { height = _height; }return *this;
+	}
+
+	Planet& setWeight(double _weight)
+	{
+		if (_weight > 0 && _weight < 1000) { weight = _weight; } return *this;
+	}
+
 };
 
+
+void planetSort(std::vector<Planet>& planets)
+{
+	for (int i = 0; i < planets.size() - 1; i++)
+	{
+		for (int j = 0; j < planets.size() - i - 1; j++)
+		{
+			if (planets.at(j).getWeight() > planets.at(j + 1).getWeight())
+			{
+				std::swap(planets.at(j), planets.at(j + 1));
+			}
+		}
+	}
+}
 
 int main()
 {
 	srand(time(0));
 	system("chcp 1251");
 	std::vector<Planet> planet;
-	for (int i = 0; i < 10; i++)
+
+
+	std::cout << std::endl;
+
+
+	for (int i = 0; i < 9; i++)
 	{
-		planet.push_back(Planet("Kolobojskaya", rand() % 10, rand() % 10, rand() % 10));
+		planet.push_back(Planet("Kolobojskaya",  rand() % 10, rand() % 10, rand() % 10));
 	}
+
+	std::cout << "ƒо сортировки по ширине:" << std::endl;
 
 	for (int i = 0; i < planet.size(); i++)
 	{
-		planet.at(i).printInfo(rand()% 100);
+		planet.at(i).printInfo();
+	}
+
+	planetSort(planet);
+
+
+	for (int i = 0; i < 100; i++) std::cout << "-"; //распечатка границы
+
+	std::cout << std::endl;
+
+	std::cout << "ѕосле сортировки по ширине:" << std::endl;
+
+	for (int i = 0; i < planet.size(); i++)
+	{
+		planet.at(i).printInfo();
 	}
 	return 0;
 }
